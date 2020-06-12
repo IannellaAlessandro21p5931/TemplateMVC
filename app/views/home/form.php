97171f3a64596b19bd907aab52fe9e1dac0bbcfb
21p5931IannellaAlessandro <21p5931@bztf.ch>
2020-06-12 11:37:03 +0200

    Freitag_11.36
    Signed-off-by: 21p5931IannellaAlessandro <21p5931@bztf.ch>

--- app/views/home/form.php
+++ app/views/home/form.php
@@ -2,40 +2,70 @@
 
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
 
-    <div style="padding: 20px;">
-        <select name="Von" id="von">
-            <?php
-                $trainstations = $data['trainstations'];
-                foreach($trainstations as $key => $value):
-                echo '<option value="'.$key.'">'.$value.'</option>';
-                endforeach;
-            ?>
-        </select>
-    </div>
+    <div class="container">
+        <div class="row" style="margin-top: 2%">
+            <div class="col-xs-6" style="margin-top: 5%">
+                
+                <div style="padding: 20px;">
+                    <select style="width: 35%" name="von">
+                        <option selected disabled>Abreiseort</option>
+                        <?php
+                            $trainstations = $data['trainstations'];
+                            foreach($trainstations as $key => $value):
+                            echo '<option value="'.$key.'">'.$value.'</option>';
+                            endforeach;
+                        ?>
+                    </select>
+                </div>
 
-    <div style="padding: 20px;">
-        <select name="Nach" id="nach">
-            <?php
-                $trainstations = $data['trainstations'];
-                foreach($trainstations as $key => $value):
-                echo '<option value="'.$key.'">'.$value.'</option>';
-                endforeach;
-            ?>
-        </select>
-    </div>
+                <div style="padding: 20px;">
+                    <select style="width: 35%" name="nach">
+                        <option selected disabled>Ziel</option>
+                        <?php
+                            $trainstations = $data['trainstations'];
+                            foreach($trainstations as $key => $value):
+                            echo '<option value="'.$key.'">'.$value.'</option>';
+                            endforeach;
+                        ?>
+                    </select>
+                </div>
 
-    <div style="padding: 20px;">
-        <input type="date" id="datum" name="datum"></input>
+                <div style="padding: 20px; margin-top: 7%">
+                    <input style="width: 35%" type="date" name="datum"></input>
+                </div>
+
+                <div style="padding: 20px;">
+                    <input style="width: 35%" type="text" name="anz" placeholder="Anzahl Billets"></input>
+                </div>
+
+            </div>
+
+            <div class="col-xs-6" style="margin-top: 5%;">
+                <div style="padding: 20px;">
+                    Halbtax
+                    <p style="padding: 5px; margin-top: 5px;">
+                    <input type="radio" name="halbtax"> Ja</input>
+                    </p>
+                    <p style="padding: 5px;">
+                    <input type="radio" name="halbtax" checked> Nein</input>
+                    </p>
+                </div>
+
+                <div style="padding: 20px;">
+                    Klasse
+                    <p style="padding: 5px; margin-top: 5px;">
+                    <input type="radio" name="klasse"> 1. Klasse</input>
+                    </p>
+                    <p style="padding: 5px;">
+                    <input type="radio" name="klasse" checked> 2. Klasse</input>
+                    </p>
+                </div>
+            </div>
+        </div>
     </div>
     
-    <div style="padding: 20px;">
-        <button type="submit">Submit</button>
+    <div style="padding: 20px; margin-top: 5%">
+        <button style="width: 100%; background-color: #0099ff" class="btn btn-primary" type="submit">Submit</button>
     </div>
-
-
-
-    <!-- 
-        
-    -->
 
 </form>
--- app/views/home/start.php
+++ app/views/home/start.php
@@ -3,7 +3,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
 
     <div style="padding: 20px;">
-        <a class="btn btn-primary btn-lg" onclick="location.href='http://localhost/TemplateMVC/public/home/auswahl'" role="button">Start</a>
+        <a class="btn btn-primary btn-block" style="background-color: #0099ff; padding: 20%; margin-top: 10px" onclick="location.href='http://localhost/TemplateMVC/public/home/auswahl'" role="button">Start</a>
     </div>
 
 </form>
